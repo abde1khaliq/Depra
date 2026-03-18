554b80974b3530019e96ffd2011c59880ef50174
abde1khaliq <ahmedthek3@gmail.com>
2026-03-18 20:27:28 +0200

    Add docs versioning for depra

--- src/core/main.py
+++ src/core/main.py
@@ -4,10 +4,10 @@ from api.login_route.endpoints import router as login_router
 from api.register_route.endpoints import router as register_router
 import uvicorn
 
-title = "DEPRA - Deprecation Enforcement Proxy for retired APIs"
+title = "Deprecation Enforcement Proxy for retired APIs (DEPRA)"
 
 description = """
-The Core problem it solves -> Let's say a company by the name Payd built POST /v1/charge 3 years ago,
+Let's say a company by the name Payd built POST /v1/charge 3 years ago,
 It accepted raw card numbers - a security liability.
 They built POST /v2/charge as the proper replacement.
 But they have around 340 clients that still use the /v1 endpoint and they have no idea who exactly is calling it,
@@ -20,9 +20,9 @@ It intercepts all traffic, tracks who calls what, warns consumers before depreca
 and enforces hard cutoffs automatically. The API Company configures it once and the system runs itself.
 """
 
-app = FastAPI(title=title, description=description, summary=summary)
-app.include_router(login_router, prefix="/auth", tags=['authentication'])
-app.include_router(register_router, prefix="/auth", tags=['authentication'])
+app = FastAPI(title=title, description=description, summary=summary, version='v1.0')
+app.include_router(login_router, prefix="/auth", tags=['Authentication'])
+app.include_router(register_router, prefix="/auth", tags=['Authentication'])
 
 if __name__ == "__main__":
     uvicorn.run('main:app', port=8000, reload=True)
