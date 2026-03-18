c4de62ad3a8a0cd0ffc964c91e9135fcc19d9d3c
abde1khaliq <ahmedthek3@gmail.com>
2026-03-18 11:12:06 +0200

    Create User model

---
+++ src/core/database/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, null
+from sqlalchemy.orm import relationship
+from database.session import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False, index=True)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
